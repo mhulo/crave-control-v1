357eace015bb07ff7b2f7b5fb960bc1ba0c255f4
mhulo <38652531+mhulo@users.noreply.github.com>
2018-04-28 18:50:59 +1000

    Update index.php

--- code/index.php
+++ code/index.php
@@ -1,11 +1,13 @@
 <?php 
 
-//echo phpinfo();
-//echo "this works 7";
+// echo "php in docker works";
+// echo phpinfo();
+
+// simple function to test c-gate is working
+// sends a message to c-gate, and returns what c-gate responds with
 
 	function cbus_cgate_send_message($msg)
     {
-
         $cr_ret = chr(0x0D) . chr(0x0A);
         $cbus_cgate_project = "MYHOME";
         $cbus_cgate_network = "254";
@@ -15,7 +17,7 @@
         if ($msg == "get_all") { $msg = "get //" . $cbus_cgate_project . "/" . $cbus_cgate_network . "/" . $cbus_cgate_lighting_application . "/* level"; }
 
         $fp = fsockopen("cgate-container", "20023", $errno, $errstr);
-        if (!$fp) { $result2 = "could not connect6"; } // error: could not connect to command interface port
+        if (!$fp) { $result = "could not connect: [" . $errno . "] " . $errstr; } // error: could not connect to command interface port
         else {
 
             fgets($fp, 1024); // get the command port welcome message
@@ -23,17 +25,16 @@
 
             $last_response_line = false;
 
-            $result2 = "";
+            $result = "";
             while (!$last_response_line) {
                 $buffer = fgets($fp, 4096);
                 if (strpos($buffer,"300-//") === false) { $last_response_line = true; } // ie. the last line will not have the dash and will just be '300 //' not '300-//'
-                $result2 .= $buffer;
+                $result .= $buffer;
             }
     
             fclose($fp); // close the connection
         }
-        //return $result2 . "::" . $errno . "::" . $errstr;
-        return $result2;
+        return $result;
     }
 
 echo cbus_cgate_send_message("get_all");
