fcad703b65376947f37e7c78e6be930b10988524
mhulo <38652531+mhulo@users.noreply.github.com>
2018-04-28 18:10:47 +1000

    Update and rename install.sh to install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -9,6 +9,3 @@ sudo usermod -aG docker pi
 sudo apt-get install python-pip
 sudo pip install docker-compose
 sudo docker-compose --version
-
-sudo cp ~/cravectl-1/cravectl-1.service /etc/systemd/system/cravectl-1.service
-sudo systemctl enable cravectl-1
